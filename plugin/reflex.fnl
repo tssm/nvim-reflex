b63a772916d0c09d662353b21b67cdd9d83da002
Tae Selene Sandoval Murgan <tssm@users.noreply.github.com>
2022-03-05 16:58:41 +0100

    Remove g:reflex_delete_cmd

--- plugin/reflex.fnl
+++ plugin/reflex.fnl
@@ -6,12 +6,3 @@
 (cmd "autocmd!")
 (cmd "autocmd BufReadPost * lua require'reflex'['set-up']()")
 (cmd "augroup END")
-
-(when vim.g.reflex_delete_cmd
-  (vim.api.nvim_echo
-    [[
-      "reflex_delete_cmd will be removed, use reflex_delete_file_cmd instead"
-      :WarningMsg]]
-    true
-    {})
-  (set vim.g.reflex_delete_file_cmd vim.g.reflex_delete_cmd))
--- plugin/reflex.lua
+++ plugin/reflex.lua
@@ -16,9 +16,4 @@ cmd("augroup Reflex")
 cmd("autocmd!")
 cmd("autocmd BufReadPost * lua require'reflex'['set-up']()")
 cmd("augroup END")
-if vim.g.reflex_delete_cmd then
-  vim.api.nvim_echo({{"reflex_delete_cmd will be removed, use reflex_delete_file_cmd instead", "WarningMsg"}}, true, {})
-  vim.g.reflex_delete_file_cmd = vim.g.reflex_delete_cmd
-else
-end
 return _2amodule_2a
